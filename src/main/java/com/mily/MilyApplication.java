5120fa209294641c5506bbd383e403ee8c578b1e
nowJun82 <oizill5481@icloud.com>
2023-10-10 12:50:28 +0900

    setting

---
+++ src/main/java/com/mily/MilyApplication.java
@@ -0,0 +1,13 @@
+package com.mily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilyApplication.class, args);
+	}
+
+}
