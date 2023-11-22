09f4f44a4c22eaae5477f52b3983c820629e7dfd
nowJun82 <oizill5481@icloud.com>
2023-11-23 01:08:56 +0900

    데스크탑 작업 ver.2

--- src/main/java/com/mily/reservation/ReservationController.java
+++ src/main/java/com/mily/reservation/ReservationController.java
@@ -12,7 +12,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RequestMapping("/reservation")
 @RequiredArgsConstructor
@@ -70,8 +74,25 @@ public class ReservationController {
     @GetMapping("/select_date")
     public String selectDate(@RequestParam("lawyerUserId") Long lawyerUserId, Model model) {
         MilyUser lawyerUser = milyUserService.findById(lawyerUserId).get();
+
+        List<String> dates = new ArrayList<>();
+        List<String> daysOfWeek = new ArrayList<>();
+        LocalDate start = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
+
+        for (int i = 0; i < 7; i++) {
+            dates.add(start.plusDays(i).format(formatter));
+            String dayOfWeek = start.plusDays(i).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN);
+            daysOfWeek.add(dayOfWeek);
+        }
+
+//        String dayOfWeek = ld.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN);
+
         model.addAttribute("user", lawyerUser);
         model.addAttribute("lawyerUserId", lawyerUserId);
+        model.addAttribute("dates", dates);
+        model.addAttribute("day", daysOfWeek);
+
         return "select_date";
     }
 }
